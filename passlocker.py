0fdba1474ff0933e2d3e5e77daffb317813fe0d6
millywayne <achiengmillicent35@gmail.com>
2020-10-10 18:20:00 +0300

    generated docstring and passed in arguments

---
+++ passlocker.py
@@ -0,0 +1,74 @@
+import pyperclip
+class passlockers:
+    '''
+class that creates user account
+'''
+pass_list =[]
+
+# assign property to list
+
+def _init_(self, account, email, password):
+
+   self.account = account
+   self.email = email
+   self.password = password
+
+#    save passlock
+
+def save_pass(self):
+    '''
+    self passlockers in pass_list
+    '''
+    Passlockers.pass_list.append(self)
+
+    # delete passlock
+
+def delete_pass(self):
+    '''
+    delete passlockers
+    '''
+    Passlockers.pass_list.remove(self)
+
+    # search passlock
+
+    @classmethod
+    def find_account(cls, account):
+        '''
+        search accounts
+        '''
+        for pass in cls.pass_list:
+            if pass.account == account:
+                return pass
+
+                # confirm passlock
+
+@classmethod
+def pass_exist(cls, account):
+    '''
+    confirm if class exists
+    '''
+    for pass in cls.pass_list:
+        if pass.account == account:
+         return True
+     return False
+                
+        # display passlock
+
+@classmethod
+def display_pass(cls):
+    '''
+    method that returns all credentials
+    '''
+    return cls.pass_list
+
+    # copy password
+
+    @classmethod
+    def copy_password(cls, passlock):
+        find_account =credentials.find_account(passlock)
+        peperclip.copy(find_account.password)
+
+
+
+
+
